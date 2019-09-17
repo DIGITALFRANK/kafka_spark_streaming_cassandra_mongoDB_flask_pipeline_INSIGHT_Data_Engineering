549f5393ab8a5969075b71660cd2f49d6fc31c5d
FRANCIS AGBODJI <frank@digitalfrank.com>
2019-09-17 18:42:46 -0400

    kafka producer/consumer test 2

--- user_logs/user_logs_consumer.py
+++ user_logs/user_logs_consumer.py
@@ -12,9 +12,9 @@ user_logs_consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
 
-# adds current timestamp to every kafka message at consumption
+# according to documentation, this may drop messages that haven't yet arrived to the queue
 for message in user_logs_consumer:
-    timestamp = datetime.utcnow()
+    timestamp = datetime.utcnow()  # adds current timestamp to every kafka message at consumption
     value = message.value
     atom = json.load({"timestamp": timestamp, "value": value})
 
@@ -22,3 +22,10 @@ for message in user_logs_consumer:
     # What's your windowing?
     # How are we appending to DataFrame?
     # Sort after each window append?
+
+
+# Consuming messages in this fashion ensures no data loss, even if messages have yet to arrive to queue
+while True:
+    raw_messages = user_logs_consumer.poll(timeout_ms=1000, max_records=5000)
+    for topic_partition, message in raw_messages.items():
+        application_message = json.loads(message.value.decode())
